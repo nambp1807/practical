3c7a1c09e5577286d8a0ad48b350452a112a1153
B.P_______NAM <phuongnam744@gmail.com>
2020-03-27 16:00:48 +0700

    aaa

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function user(){
+        $user = User::all();
+        return view('user',['user'=>$user]);
+    }
+
+    public function userCreate(){
+        return view('userCreate');
+    }
+
+    public function userStore(Request $request){
+        $request->validate([
+            "name"=> "required|string",
+            "age"=> "required|Integer",
+            "address"=> "required|string",
+            "telephone"=> "required|string"]);
+
+        try{
+            User::create([
+                "name"=> "required|string",
+                "age"=> "required|Integer",
+                "address"=> "required|string",
+                "telephone"=> "required|string"
+            ]);
+        }catch(\Exception $e){
+            return redirect()->back();
+        }
+        return redirect()->to("user");
+    }
+
+}
---
+++ database/migrations/2020_03_27_081943_create_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->unsignedBigInteger('age');
+            $table->string("address");
+            $table->string("telephone");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
